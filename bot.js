adb2405da84fe16c248e95f159dff8f442df6a86
sudosoup <sudosoup@gmail.com>
2018-09-22 11:34:50 -0700

    bing bong reply

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 			case 'ping':
 				bot.sendMessage({
 					to: channelID,
-					message: 'Pong!'
+					message: 'Bing Bong!'
 				});
 			break;
 			// Just add any case commands if you want to..
